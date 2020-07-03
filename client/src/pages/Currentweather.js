5391eb18aaf71fee496d53f6be908cec04bce836
ayus91 <jihaleeb1@gmail.com>
2020-07-04 02:02:15 +0900

    Add templete

---
+++ client/src/pages/Currentweather.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Currentweather extends React.Component {
+    render() {
+        return (
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Currentweather</h5>
+                </div>
+            </div>
+        );
+    }
+}
+
+export {Currentweather}
