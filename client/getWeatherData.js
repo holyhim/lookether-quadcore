b345033138d2f8bd3217023ffec97a795b50fc33
ayus91 <jihaleeb1@gmail.com>
2020-07-04 01:59:54 +0900

    Undo weather data

--- client/getWeatherData.js
+++ client/getWeatherData.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { WEATHER_API_KEY } from "./weatherAPI_KEY/WeatherAPI_KEY";
+// import { WEATHER_API_KEY } from "./weatherAPI_KEY/WeatherAPI_KEY";
 
 
 const getWeatherData = ({ lat, lon, key }, callback) => {
@@ -25,34 +25,4 @@ const reverseGeo = ({ lat, lon }, callback) => {
 };
 
 
-
-const getCurrentWeather = (callback) => {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition((position) => {
-			getWeatherData(
-				{
-					lat: position.coords.latitude,
-					lon: position.coords.longitude,
-					key: WEATHER_API_KEY,
-				},
-				(data) => {
-					callback(data);
-				}
-			);
-			reverseGeo(
-				{
-					lat: position.coords.latitude,
-					lon: position.coords.longitude,
-				},
-				(data) => {
-					callback(data);
-				}
-			)
-		});
-	} else {
-		alert("현재 위치 정보가 현재 브라우저에서 지원하지 않습니다.");
-	}
-}
-
-
-export { getWeatherData , reverseGeo , getCurrentWeather } 
+export { getWeatherData , reverseGeo } 
