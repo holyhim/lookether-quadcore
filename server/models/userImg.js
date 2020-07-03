03b83c592134e28ec55e8ed328a211bd26de8db7
khgkimhyungyu <yolo-fabian@naver.com>
2020-07-03 15:13:29 +0900

    Completed to design models

--- server/models/userImg.js
+++ server/models/userImg.js
@@ -1,22 +1,30 @@
-// TODO: userImg에 필요한 스키마 정의
-
-//* imgId, like  --> database
-
-
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  const userImg = sequelize.define(
-    'userImg',
-    { // email,password,username,mobile
-      //TODO : user 테이블에 필요한 스키마를 정의 하세요
-      imgID: DataTypes.INTEGER,
-      img: DataTypes.BLOB,
-      like: DataTypes.INTEGER
-    },
-    {}
-  );
-  userImg.associate = function(models) {
-    // associations can be defined here
+  class userImg extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
   };
+  userImg.init({
+    imgID: DataTypes.INTEGER,
+    img: DataTypes.BLOB,
+    like: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'userImg',
+  });
   return userImg;
 };
 
+userImg.associate = function(models) {
+  // associations can be defined here
+  userImg.belongsTo(models.userInfo, { foreignKey: 'imgID'})
+};
--- server/models/userInfo.js
+++ server/models/userInfo.js
@@ -1,22 +1,31 @@
-// TODO userinfo에 필요한 스키마정의
-
-// * username, email, gender, password
-
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    const userInfo = sequelize.define(
-      'userInfo',
-      { // email,password,username,mobile
-        //TODO : userinfo에 테이블에 필요한 스키마를 정의 하세요
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-        username: DataTypes.STRING,
-        gender: DataTypes.STRING
-      },
-      {}
-    );
-    userInfo.associate = function(models) {
-      // associations can be defined here
-    };
-    return userInfo;
+  class userInfo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
   };
-  
+  userInfo.init({
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    username: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'userInfo',
+  });
+  return userInfo;
+};
+
+userInfo.associate = function(models) {
+  // associations can be defined here
+  userInfo.hasMany(models.userImg, { foreignKey: 'imgID'})
+};
