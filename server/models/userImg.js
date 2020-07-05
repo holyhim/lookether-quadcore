d6caecc06243f80851ab7f2d6aac0e7253455ce7
khgkimhyungyu <yolo-fabian@naver.com>
2020-07-05 12:24:33 +0900

    Updated models

--- server/models/userImg.js
+++ server/models/userImg.js
@@ -3,28 +3,23 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class userImg extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+  const userImg = sequelize.define(
+    'userImg',
+    {
+      imgID: DataTypes.INTEGER,
+      img: DataTypes.BLOB,
+      like: DataTypes.INTEGER
+    },
+    {}
+  );
+  userImg.associate = function(models) {
+    // associations can be defined here
+    // userImg.belongsTo(models.userInfo, { foreignKey: 'imgID'})
   };
-  userImg.init({
-    imgID: DataTypes.INTEGER,
-    img: DataTypes.BLOB,
-    like: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'userImg',
-  });
-  return userImg;
+  return userImg
 };
 
-userImg.associate = function(models) {
-  // associations can be defined here
-  userImg.belongsTo(models.userInfo, { foreignKey: 'imgID'})
-};
+// userImg.associate = function(models) {
+//   // associations can be defined here
+//   userImg.belongsTo(models.userInfo, { foreignKey: 'imgID'})
+// };
--- server/models/userInfo.js
+++ server/models/userInfo.js
@@ -3,29 +3,23 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class userInfo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  userInfo.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    gender: DataTypes.STRING,
-    username: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'userInfo',
-  });
-  return userInfo;
+  const userInfo= sequelize.define(
+    'userinfo',
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      gender: DataTypes.STRING,
+      username: DataTypes.STRING
+    },
+    {}
+  );
+  userInfo.associate = function(models) {
+    // userInfo.hasMany(models.userImg, { foreignKey: 'id'})
+  }
+  return userInfo
 };
 
-userInfo.associate = function(models) {
-  // associations can be defined here
-  userInfo.hasMany(models.userImg, { foreignKey: 'imgID'})
-};
+// userInfo.associate = function(models) {
+//   // associations can be defined here
+//   userInfo.hasMany(models.userImg, { foreignKey: 'imgID'})
+// };
