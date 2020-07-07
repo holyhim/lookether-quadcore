879b4993f20ed600998e750fffd2ff4a362f64f4
ayus91 <jihaleeb1@gmail.com>
2020-07-07 14:16:28 +0900

    Fix few things

--- client/src/pages/Contentpage.js
+++ client/src/pages/Contentpage.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React from "react"
 import { Currentweather } from "./Currentweather"
-import { Todayclothes } from "./Todayclothes"
+import Todayclothes from "./Todayclothes"
 import { Weekweather } from "./Weekweather"
-import  "./Contentpage.css"
+import "./Contentpage.css"
 
-const Contentpage = () => (
-  <div>
-	<div class="row">
-    <div class="cur-weather"><Currentweather /></div>
-    <div class="cloth"><Todayclothes /></div>
-  </div>
-  <div class="row">
-    <div class="weekweather"><Weekweather /></div>
-  </div>
+const uniqid = require("uniqid")
+
+/*
+props = {
+  weatherInfo:{
+    icon: "",
+		temp: "",
+		feelslike: "",
+		min: "",
+		max: "",
+		rain: "",
+  }
+}
+*/
+
+const Contentpage = (props) => (
+	<div>
+		<div class="row">
+			<div class="cur-weather">
+				<Currentweather weather={props.weather} />
+			</div>
+			<div class="clothe">
+				<Todayclothes weather={props.weather.temp} />
+			</div>
+		</div>
+		<div class="row">
+			<div class="weekweather">
+				{props.daily.map((data) => (
+					<Weekweather
+						key={uniqid(`${data.weather[0].id}-`)}
+						weather_id={data.weather[0].id}
+						max={data.temp.max}
+						min={data.temp.min}
+						icon={data.weather[0].icon}
+					/>
+				))}
+			</div>
+		</div>
 	</div>
 )
 
-export  {Contentpage};
+export { Contentpage }
